1ba42bfeb556d70d865e55bc5c2cb3749351c51b
Spencer Palmeter <spencerpalmeter@gmail.com>
2019-03-24 20:57:24 -0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,71 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "bob";
+        String t = "betty";
+        String u = "Chuck";
+
+        //compareTo gives <0, 0, or >0
+        System.out.println("bob.compareTo(betty) is " + s.compareTo(t));
+        System.out.println("s.compareToIgnoreCase(u) is " + s.compareToIgnoreCase(u));
+        if(s.compareTo(t) < 0) {
+            System.out.println("Less");
+        } else {
+            System.out.println("More");
+        }
+
+        Person a = new Person("Fred", 28);
+        Person b = new Person("Barney", 27);
+        Person c = new Person("Wilma");
+        Person d = new Person("Betty");
+        Person e = new Person(12);
+        Person f = new Person();
+
+        System.out.println(a.toString());
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(e.compareTo(f));
+
+        //Reading into an array
+        int max = 10;
+        Person[] crowd = new Person[max];
+        int n = 0;
+        n =readPeople(crowd, max);
+        print(crowd, n);
+
+    }
+
+    private static int readPeople(Person[] crowd, int limit) {
+        int n = 0;
+        try {
+            Scanner input = new Scanner(new File("people.txt"));
+            while(input.hasNextLine() && n < limit) {
+                String line = input.nextLine();
+                String[] parts = line.split("\\t");
+                if(parts.length == 4) {
+                    String name = parts[0];
+                    int age = (int)Math.round(Double.parseDouble(parts[3])/12);
+                    Person pn = new Person(name, age);
+                    crowd[n] = pn;
+                    n++;
+                }
+            }
+            input.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return n;
+    }
+    public static void print(Person[] crowd, int n) {
+        System.out.printf("-------- %d --------\n", n);
+            for(int i=0; i<n; i++) {
+                System.out.printf("[%2d] %s\n", i, crowd[i]);
+            }
+    }
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,59 @@
+public class Person implements Comparable<Person> {
+    private String name;
+    private int age;
+
+    //constructors
+    public Person(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+    public Person(String name) {
+        this(name, 0);
+    }
+    public Person(int age) {
+        this("ANONYMOUS", age);
+    }
+    public Person() {
+        this("ANONYMOUS",0);
+    }
+
+    //setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        if(age > 120)
+            this.age = 120;
+        else
+            this.age = age;
+    }
+
+    //getters
+    //no parameters means that we do not need 'this.'
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    //toString method   @Override for documentation
+    @Override
+    public String toString() {
+        //any of these methods work and do the same thing
+        String result = String.format("Name: %s, Age: %d", name, age);
+        //String result = String.format("Name: %s, Age: %d", this.name, this.age);
+        //String result = String.format("Name: %s, Age: %d", getName(), getAge());
+        //String result = String.format("Name: %s, Age: %d", this.getName(), this.getAge());
+
+        return result;
+    }
+
+    public int compareTo(Person other) {
+        int result = this.name.compareToIgnoreCase(other.name);
+        //If the same name, break the tie on age.
+        if(result == 0)
+            result = this.age - other.age;
+        return result;
+    }
+}
